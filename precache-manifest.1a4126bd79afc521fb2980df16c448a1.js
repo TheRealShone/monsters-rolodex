f984915c0ea98fd8de4b06743be9de84ad457fa1
seijurodeku <shubhamkarkisk@gmail.com>
2019-09-13 13:49:50 +0530

    Updates

---
+++ precache-manifest.1a4126bd79afc521fb2980df16c448a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e93b08f82ffce9efc574935f10c4fd8",
+    "url": "/TheRealShone/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e11333c11a04ebfe2c9b",
+    "url": "/TheRealShone/monsters-rolodex/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "783bb2d55f15e42d1998",
+    "url": "/TheRealShone/monsters-rolodex/static/js/2.f3e65479.chunk.js"
+  },
+  {
+    "revision": "e11333c11a04ebfe2c9b",
+    "url": "/TheRealShone/monsters-rolodex/static/js/main.52762ab9.chunk.js"
+  },
+  {
+    "revision": "d536c94ed6e4ed27283e",
+    "url": "/TheRealShone/monsters-rolodex/static/js/runtime~main.b74e7053.js"
+  }
+]);
